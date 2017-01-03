e53604db5de6b5ddc3c732d083ed47430bc83ee4
Daniel Hackett <dchackett@huron.colorado.edu>
2017-01-03 10:01:55 -0700

    To avoid adding taxi/ to PYTHONPATH, hardcode install directory in relaunch.py as .. from relaunch.py's directory

--- tools/relaunch.py
+++ tools/relaunch.py
@@ -7,6 +7,11 @@ Created on Fri Dec 16 17:52:23 2016
 
 import os, sys
 import sqlite3
+
+# HACKY: Localization, make sure we can find taxi root dir
+install_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # /taxi/tools/../
+sys.path.insert(0, install_dir)
+
 import argparse
 
 ### Parse command line arguments
@@ -14,7 +19,7 @@ parser = argparse.ArgumentParser(description="Workflow taxi relauncher")
 
 parser.add_argument('--forest', type=str, required=True, help='Forest file to look for taxis in.')
 parser.add_argument('--dwork',  type=str, default=None, help='Work directory for taxi.')
-parser.add_argument('--dshell', type=str, default=None, help='Directory to find taxi.sh in (default: same as relaunch.py)')
+#parser.add_argument('--dshell', type=str, default=None, help='Directory to find taxi.sh in (default: .. from relaunch.py)')
 parser.add_argument('--taxi', type=str, default=None, help='Taxi to launch.  If not provided, launch all initial taxis.')
 parser.add_argument('--launch', dest='launch', action='store_true', help='If provided, launch taxis. If not provided, list taxis.')
 parser.set_defaults(launch=False)
@@ -27,10 +32,11 @@ forest_file = os.path.abspath(parg.forest)
 if not os.path.exists(forest_file):
     raise Exception("Specified forest {fn} does not exist".format(fn=forest_file))
     
-if parg.dshell is None:
-    taxi_shell_script = os.path.dirname(os.path.abspath(__file__)) + '/taxi.sh'
-else:
-    taxi_shell_script = os.path.abspath(parg.dshell) + '/taxi.sh'
+#if parg.dshell is None:
+#    taxi_shell_script = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/taxi.sh' # ../taxi.sh
+#else:
+#    taxi_shell_script = os.path.abspath(parg.dshell) + '/taxi.sh'
+taxi_shell_script = install_dir + '/taxi.sh'
 if not os.path.exists(taxi_shell_script):
     raise Exception("{fn} does not exist.".format(fn=taxi_shell_script))
 
