95a16d68e6fe8bb4b0638bc06ed1b792c1c84cc9
Daniel Hackett <dchackett@huron.colorado.edu>
2017-01-30 13:04:00 -0700

    Summary tool provides information about which jobs are ready to run, but not active yet

--- tools/summary.py
+++ tools/summary.py
@@ -12,8 +12,11 @@ parser.add_argument('--failed', dest='failed', action='store_true', help='Print 
 parser.set_defaults(failed=False)
 parser.add_argument('--active', dest='active', action='store_true', help='Print details of active tasks.')
 parser.set_defaults(active=False)
+parser.add_argument('--ready', dest='ready', action='store_true', help='Print details of tasks that are ready but not yet active.')
+parser.set_defaults(ready=False)
 parser.add_argument('--taxis', dest='taxis', action='store_true', help='Find which taxis are associated with active tasks.')
 parser.set_defaults(taxis=False)
+
 
 
 parg = parser.parse_args(sys.argv[1:]) # Call like "python taxi.py ...args..."
@@ -52,17 +55,23 @@ tasks = task_dict
 # Find blocked tasks
 for task_id, task in tasks.items():
     task['blocked'] = False
+    task['ready'] = False
     
     if not task.has_key('depends_on') or task['depends_on'] is None:        
         continue
 
-    failed_parent = False    
+    failed_parent = False
+    parents_all_complete = True
     for dep_id in task['depends_on']:
         if tasks[dep_id]['status'] == 'failed':
             failed_parent = True
+        if tasks[dep_id]['status'] != 'complete':
+            parents_all_complete = False
             
     if failed_parent:
         task['blocked'] = True
+    elif parents_all_complete:
+        task['ready'] = True
 
 # Sort tasks in to interesting piles
 pending_tasks  = []
@@ -70,6 +79,7 @@ active_tasks   = []
 complete_tasks = []
 failed_tasks   = []
 blocked_tasks  = []
+ready_tasks    = []
 
 for task_id, task in tasks.items():
     if task['status'] == 'pending':
@@ -82,6 +92,8 @@ for task_id, task in tasks.items():
         failed_tasks.append(task)
     elif task['blocked']:
         blocked_tasks.append(task)
+    elif task['ready']:
+        ready_tasks.append(task)
 
 
 ## Output
@@ -113,10 +125,19 @@ if parg.failed:
 else:
     print "FAILED TASKS:", [task['id'] for task in failed_tasks]
     
+if parg.ready:
+    print "READY TASKS"
+    for task in ready_tasks:
+        print "TASK", task['id']
+        pretty_print_dict(task)
+        print ""
+else:
+    print "READY TASKS:", [task['id'] for task in ready_tasks]
+    
 if parg.taxis:
     print "ACTIVE TAXIS"
     for task in active_tasks:
         print task['by_taxi'], task['id']
         
 # Print summary
-print "PENDING %d  ACTIVE %d  COMPLETE %d  FAILED %d  BLOCKED %d"%(len(pending_tasks), len(active_tasks), len(complete_tasks), len(failed_tasks), len(blocked_tasks))
+print "PENDING %d  ACTIVE %d  COMPLETE %d  FAILED %d  BLOCKED %d  READY %d"%(len(pending_tasks), len(active_tasks), len(complete_tasks), len(failed_tasks), len(blocked_tasks), len(ready_tasks))
