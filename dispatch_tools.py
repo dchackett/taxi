82844a116d2ebe1e4e7bb59b7beb7e3bb93e4b96
Daniel Hackett <dchackett@huron.colorado.edu>
2017-03-16 20:52:25 -0600

    HMC stream convenience function can be told to use Hasenbuch, but does not by default

--- dispatch_tools.py
+++ dispatch_tools.py
@@ -29,7 +29,8 @@ def mkdir_p(path):
 def make_hmc_job_stream(Ns, Nt, beta, k4, k6, N_configs, nsteps, starter, req_time,
                          start_count=0, N_traj=10, N_traj_safe=5,
                          gammarat=125., label='1',
-                         nsteps_gauge=6, streamseed=None, seeds=None,
+                         nsteps2=None, shift=0.2, nsteps_gauge=6,
+                         streamseed=None, seeds=None,
                          enable_metropolis=True, hmc_class=HMCJob):
 
     assert issubclass(hmc_class, HMCJob), "hmc_class must be an HMCJob or a subclass thereof, not {hmc}".format(hmc=str(hmc_class))
@@ -48,7 +49,8 @@ def make_hmc_job_stream(Ns, Nt, beta, k4, k6, N_configs, nsteps, starter, req_ti
             
         new_job = hmc_class(Ns=Ns, Nt=Nt, beta=beta, gammarat=gammarat, k4=k4, k6=k6,
                          label=label, count=count, req_time=req_time, N_traj=N_traj,
-                         N_traj_safe=N_traj_safe, nsteps=nsteps, nsteps_gauge=nsteps_gauge,
+                         N_traj_safe=N_traj_safe, nsteps=nsteps, nsteps2=nsteps2,
+                         shift=shift, nsteps_gauge=nsteps_gauge,
                          starter=starter, seed=job_seed, enable_metropolis=enable_metropolis)
         if isinstance(starter, HMCJob):
             new_job.depends_on.append(starter)
