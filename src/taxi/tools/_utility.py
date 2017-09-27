0fdae9d5b1a16a37747e99d65026b6ccb13faa01
Dan Hackett <daniel.hackett@colorado.edu>
2017-09-27 16:29:26 -0600

    Abandoned tasks are marked abandoned instead of failed (new status)

--- src/taxi/tools/_utility.py
+++ src/taxi/tools/_utility.py
@@ -8,11 +8,14 @@ def classify_abandoned_tasks(tasks, queue):
     abandoned_tasks = []
     
     # Find abandoned tasks
-    for task in [t for t in tasks.values() if t.status == 'active']:
-        running_taxi_status = queue.report_taxi_status_by_name(task.by_taxi)
-        if running_taxi_status != 'R':
+    for task in [t for t in tasks.values() if t.status in ('active', 'abandoned')]:
+        if task.status == 'abandoned':
             abandoned_tasks.append(task)
         else:
-            active_tasks.append(task)
+            running_taxi_status = queue.report_taxi_status_by_name(task.by_taxi)
+            if running_taxi_status != 'R':
+                abandoned_tasks.append(task)
+            else:
+                active_tasks.append(task)
             
     return active_tasks, abandoned_tasks
