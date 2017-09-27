67cd066bbb60c44133d98480212196bbac2bc06b
Dan Hackett <daniel.hackett@colorado.edu>
2017-09-27 14:57:35 -0600

    tools.load won't create specified DB files that don't exist

--- src/taxi/tools/load.py
+++ src/taxi/tools/load.py
@@ -3,11 +3,15 @@
 """
 """
 
+import os
+
 from taxi.dispatcher import SQLiteDispatcher
 from taxi.pool import SQLitePool
 
 def dispatch(db_path):
+    assert os.path.exists(db_path), 'Specified dispatch file {0} does not exist'.format(db_path)
     return SQLiteDispatcher(db_path=db_path)
 
 def pool(db_path, pool_name=None):
+    assert os.path.exists(db_path), 'Specified dispatch file {0} does not exist'.format(db_path)
     return SQLitePool(db_path, pool_name)
