14ac7c91c002c604893feda049590d507d361d71
Dan Hackett <daniel.hackett@colorado.edu>
2017-09-27 15:18:01 -0600

    tools.load.dispatch_and_pool to load Dispatch and Pool DBs provided in either order as CLAs. taxi-edit loads pool if provided.

--- src/taxi/tools/load.py
+++ src/taxi/tools/load.py
@@ -15,3 +15,35 @@ def dispatch(db_path):
 def pool(db_path, pool_name=None):
     assert os.path.exists(db_path), 'Specified dispatch file {0} does not exist'.format(db_path)
     return SQLitePool(db_path, pool_name)
+
+def dispatch_and_pool(*db_paths):
+    """Loads Dispatch and Pool DBs.  Figures out which path is which by trial and error. Returns dispatch, pool.
+    
+    Doesn't work with pool DBs with more than one pool. Ignores any path after second specified.
+    """
+    
+    if len(db_paths) == 1:
+        d = None
+        p = None
+        
+        try:
+            d = dispatch(db_paths[0])
+        except:
+            d = None
+            
+        try:
+            p = pool(db_paths[0])
+        except:
+            d = None
+    
+    elif len(db_paths) > 1:
+        # Try first two
+        try:
+            d = dispatch(db_paths[0])
+            p = pool(db_paths[1])
+        except:
+            d = dispatch(db_paths[1])
+            p = pool(db_paths[0])
+        
+        
+    return d,p
