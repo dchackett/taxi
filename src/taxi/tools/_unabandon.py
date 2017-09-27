0e022cca77bdcebbf4d3ea8375cb3df9964d9a8a
Daniel Hackett <dhackett@bc1.fnal.gov>
2017-09-27 17:43:45 -0500

    Fix taxi-unabandon

--- src/taxi/tools/_unabandon.py
+++ src/taxi/tools/_unabandon.py
@@ -11,7 +11,7 @@ def unabandon(dispatch, pool, rollback_dir=None, delete_files=False):
     ## Connect to queue to check taxi existence
     q = LocalQueue()
     
-    pool.update_all_taxis_queue_status() # Marks tasks as abandoned, updates taxis as missing
+    pool.update_all_taxis_queue_status(dispatcher=dispatch) # Marks tasks as abandoned, updates taxis as missing
     
     tasks = dispatch.get_task_blob(include_complete=True) # Task blob is an id : task dict
     active_tasks, abandoned_tasks = _utility.classify_abandoned_tasks(tasks=tasks, queue=q)
@@ -19,8 +19,10 @@ def unabandon(dispatch, pool, rollback_dir=None, delete_files=False):
     relevant_taxis = []
     for task in abandoned_tasks:
         print "UNABANDONING", task.id, task
+        task.status = 'abandoned'
+        dispatch.write_tasks([task])
         dispatch.rollback(task, rollback_dir=rollback_dir, delete_files=delete_files)
-        q.update_taxi_status(my_taxi=task.by_taxi, status='I')
+        pool.update_taxi_status(my_taxi=task.by_taxi, status='I')
         relevant_taxis.append(task.by_taxi)
     
     print "RELAUNCHING MISSING TAXIS", relevant_taxis
