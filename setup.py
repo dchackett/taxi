2353473893f0e91f3f15994802f72b7e7f346cf1
Dan Hackett <daniel.hackett@colorado.edu>
2017-09-27 12:42:06 -0600

    Bash script 'taxi-summary' to call taxi.tools.summary installs to $PATH

--- setup.py
+++ setup.py
@@ -96,5 +96,5 @@ setuptools.setup(
     packages=setuptools.find_packages('src'),
     
     # Make taxi.sh and run_taxi.py available to run everywhere
-    scripts=['bin/taxi.sh', 'bin/run_taxi.py']
+    scripts=['bin/taxi.sh', 'bin/run_taxi.py', 'src/taxi/tools/taxi-summary']
 )
