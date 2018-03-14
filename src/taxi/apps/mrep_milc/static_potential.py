b473b0e72578b4b2a1d435b3e0b0c66a5d0c12a8
Dan Hackett <daniel.hackett@colorado.edu>
2018-03-13 19:08:50 -0600

    Fix starter->measure_on in flow.py, static_potential.py docstrings in mrep_milc application

--- src/taxi/apps/mrep_milc/static_potential.py
+++ src/taxi/apps/mrep_milc/static_potential.py
@@ -6,7 +6,7 @@ from taxi.file import File, InputFile
 # - "flow_binary"
 
 
-flow_input_template = """
+hvy_qpot_input_template = """
 prompt 0
     
 nx {Ns}
@@ -14,11 +14,7 @@ ny {Ns}
 nz {Ns}
 nt {Nt}
     
-epsilon {epsilon}
-tmax {tmax}
-minE {minE}
-mindE {mindE}
-{xi_str}
+off_axis_flag 1
     
 reload_serial {loadg}
 forget
@@ -27,27 +23,23 @@ EOF
 """
 
 
-class FlowTask(ConfigMeasurement):
+class StaticPotentialTask(ConfigMeasurement):
     ## File naming conventions
     loadg = InputFile('{loadg_filename_prefix}_{Ns:d}_{Nt:d}_{beta:g}_{k4:g}_{k6:g}_{label}_{traj:d}')
     fout = File('{fout_filename_prefix}_{Ns:d}_{Nt:d}_{beta:g}_{k4:g}_{k6:g}_{label}_{traj:d}')
-    fout_filename_prefix = 'flow'
+    fout_filename_prefix = 'qpot'
     saveg = None
     
     binary = None # Specify this in run-specification scripts
     
     def __init__(self,
-                 # Application-specific required arguments
-                 tmax,
                  # Override ConfigMeasurement defaults
                  req_time=600, 
-                 # Application-specific defaults
-                 minE=0, mindE=0.0, epsilon=0.01, xi=None,
                  # Overrides
                  Ns=None, Nt=None,
                  # Arguments to pass along to superclass
                  **kwargs):
-        """Perform the Wilson Flow on a gauge configuration.
+        """Measure static quark potential on a gauge configuration.
         
         If measure_on is a filename, attempts to read parameters from filename. If
         measure_on is a ConfigGenerator, steals parameters from the GaugeGenerator.
@@ -58,43 +50,22 @@ class FlowTask(ConfigMeasurement):
                 whatever was found in the ConfigGenerator or filename.
             Nt: Number of lattice points in temporal direction. If provided, overrides
                 whatever was found in the ConfigGenerator or filename.
-            epsilon: Step size for numerical integration of flow
-            tmax: Terminate flow at t=tmax.  If tmax=0, uses adaptive flow stopping.
-            minE: Terminate flow when <t^2 E(t)> >= minE
-            mindE: Terminate flow when <t d/dt t^2 E(t)> >= mindE
         """
         
-        super(FlowTask, self).__init__(req_time=req_time, **kwargs)
-
-        # Physical parameters
-        self.tmax = tmax
-        self.minE = minE
-        self.mindE = mindE
-        self.epsilon = epsilon
-        self.xi = xi
+        super(StaticPotentialTask, self).__init__(req_time=req_time, **kwargs)
         
         # Override parameters read out from a filename or stolen from a ConfigGenerator
         if Ns is not None:
             self.Ns = Ns
         if Nt is not None:
             self.Nt = Nt
-
-        # Don't run trivial flows
-        if tmax == 0:
-            assert minE != 0 or mindE != 0, \
-                "If tmax is 0, must set adaptive flow parameters minE and mindE or flow will be trivial"
         
     
     def build_input_string(self):
-        input_str = super(FlowTask, self).build_input_string()
+        input_str = super(StaticPotentialTask, self).build_input_string()
         
         input_dict = self.to_dict()
-        
-        if self.xi is not None:
-            input_dict['xi_str'] = 'xi {xi}'.format(xi=self.xi)
-        else:
-            input_dict['xi_str'] = ''
 
-        return input_str + flow_input_template.format(**input_dict)
+        return input_str + hvy_qpot_input_template.format(**input_dict)
     
 
